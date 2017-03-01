ed1012b9c425b0fbd2a63dea62f5835f2c95eb82
Terence Sun <tsun1215@gmail.com>
2017-03-01 14:40:11 -0500

    Added passing file name to plot.py

--- grideye_test/plot.py
+++ grideye_test/plot.py
@@ -1,22 +1,30 @@
 # Run this on a host computer 
 
+import sys
 import numpy
 import time
 import matplotlib.pyplot as plt
 from matplotlib import animation
 
-data = numpy.genfromtxt("data.csv", delimiter=",")
+def main():
+  if len(sys.argv) < 2:
+    print "Usage ./plot.py <csv_file_name>"
 
-fig = plt.figure()
-ax = fig.add_subplot(111)
-im = ax.imshow(data[0].reshape((8,8)))
-plt.show(block=False)
-
-
-# Show heatmap frame by frame, redrawing every 0.1 seconds (10 FPS)
-for i in range(len(data)):
-  time.sleep(0.1)
-  im.set_array(data[i].reshape((8,8)))
-  fig.canvas.draw()
+  data = numpy.genfromtxt(sys.argv[1], delimiter=",")
   
+  fig = plt.figure()
+  ax = fig.add_subplot(111)
+  im = ax.imshow(data[0].reshape((8,8)))
+  plt.show(block=False)
+  
+  
+  # Show heatmap frame by frame, redrawing every 0.1 seconds (10 FPS)
+  for i in range(len(data)):
+    time.sleep(0.1)
+    im.set_array(data[i].reshape((8,8)))
+    fig.canvas.draw()
+
+if __name__ == "__main__":
+  main()
+    
 
