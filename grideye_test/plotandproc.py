5d45910b650625290b87fb040b68dab7c127cadb
Udaya <karnmalik@MacBook-Pro-4.local>
2017-03-08 17:50:19 -0500

    Cleaned up

--- grideye_test/plotandproc.py
+++ grideye_test/plotandproc.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 MEDIAN_FILTER_SIZE = 10
 
 ser = serial.Serial()
-ser.port = "/dev/tty.usbmodem1412"
+ser.port = "/dev/tty.usbmodem1422"
 ser.baudrate = 115200
 ser.dsrdtr = True
 
@@ -37,21 +37,21 @@ fname_med = t + "-median" + ".log.csv"
 
 def count(maxVal, In, countOfFrame, countOut, countIn):
   pix = 0 if In else 7
-  print "within count"
+  # print "within count"
   if frameCount >= 10:
-    print "In count"
+    # print "In count"
     index = 0
     for m in range(3,8):
       frameToCheck = countBuff[index+m].reshape((8,8))
       countPixel = 0
-      print "set countPixel"
+      # print "set countPixel"
       for j in range(0, 8):
-        print "frameToCheck value = ", frameToCheck[j][pix]
+        # print "frameToCheck value = ", frameToCheck[j][pix]
         if frameToCheck[j][pix] >= maxVal:
-          print "within countPixel"
+          # print "within countPixel"
           countPixel += 1
           if countPixel == 3: 
-            print "within pizek == 3"
+            # print "within pizek == 3"
             if In:
               countIn += 1
             else:
@@ -63,7 +63,7 @@ def countPeople(frame, countOfFrame, countOut, countIn):
   maxValOut = 2.0
   for i in xrange(8):
     if frame[i][7] >= maxValIn:
-      print "In count people before calling CountIn. Value = ", frame[i][7]
+      # print "In count people before calling CountIn. Value = ", frame[i][7]
       (countIn, countOut) = count(maxValIn, True, countOfFrame, countOut, countIn)
       break
     if frame[i][0] >= maxValOut:
@@ -72,7 +72,8 @@ def countPeople(frame, countOfFrame, countOut, countIn):
   return (countIn, countOut)
 
 
-# current_time = datetime.datetime.now()
+current_time = datetime.datetime.now()
+last_time = current_time
 with open(fname_med, 'w') as med_file:
   with open(fname, 'w') as csv_file:
       writer = csv.writer(csv_file, delimiter=',')
@@ -98,7 +99,7 @@ with open(fname_med, 'w') as med_file:
           countBuff.append(filtered_val)
           startIndex = frameCount - 10
 
-        (countIn, countOut) = countPeople(filtered_val, frameCount, countOut, countIn)
+        # (countIn, countOut) = countPeople(filtered_val, frameCount, countOut, countIn)
         print 'countIn: ', countIn
         print 'countOut: ', countOut
 
@@ -108,7 +109,7 @@ with open(fname_med, 'w') as med_file:
 
         
         # Print FPS
-        #current_time = datetime.datetime.now()
-        #print str(1000000/(current_time - last_time).microseconds) + " FPS"
-        #last_time = current_time
+        current_time = datetime.datetime.now()
+        print str(1000000/(current_time - last_time).microseconds) + " FPS"
+        last_time = current_time
   
